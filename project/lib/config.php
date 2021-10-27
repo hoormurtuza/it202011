6c8581eb1c3cdb11f259c947ec5c62f8d627cbac
hoormurtuza <70714406+hoormurtuza@users.noreply.github.com>
2021-10-27 11:53:36 -0400

    Update config.php

--- project/lib/config.php
+++ project/lib/config.php
@@ -2,6 +2,6 @@
 
 $dbhost = "sql1.njit.edu";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
 $dbuser = "hm372";//your ucid since we follow this pattern
-$dbpass = "Honasababa@786";//database password generated or chosen per respective steps
+$dbpass = "";//database password generated or chosen per respective steps
 $dbdatabase = "hm372";//your ucid since we follow this pattern
 ?>
